32551958b6a59cec502ac903a2fcbb7a05c797a1
Young Min Kwon <yaoming950@gmail.com>
2026-05-18 19:01:49 -0500

    feat: add trending movies section to HomeView

--- MovieExplorer/MovieExplorer/Features/Home/HomeViewModel.swift
+++ MovieExplorer/MovieExplorer/Features/Home/HomeViewModel.swift
@@ -4,6 +4,7 @@ import Combine
 class HomeViewModel: ObservableObject {
     @Published var popularMovies: [Movie] = []
     @Published var topRatedMovies: [Movie] = []
+    @Published var trendingMovies: [Movie] = []
     @Published var popularTVShows: [TVShow] = []
     @Published var isLoading = false
     @Published var errorMessage = ""
@@ -16,11 +17,13 @@ class HomeViewModel: ObservableObject {
             do {
                 let movies = try await TMDBService.shared.fetchPopularMovies()
                 let topRated = try await TMDBService.shared.fetchTopRatedMovies()
+                let trending = try await TMDBService.shared.fetchTrendingMovies()
                 let shows = try await TMDBService.shared.fetchPopularTVShows()
                 
                 await MainActor.run {
                     popularMovies = movies
                     topRatedMovies = topRated
+                    trendingMovies = trending
                     popularTVShows = shows
                     isLoading = false
                 }
